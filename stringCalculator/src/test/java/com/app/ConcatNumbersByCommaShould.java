989e63f4f6f37ef3ad68897038f6848d14414636
komaljagtap1 <komaljagtap102@gmail.com>
2021-10-17 00:55:52 +0530

    first commit

---
+++ stringCalculator/src/test/java/com/app/ConcatNumbersByCommaShould.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.junit.Test;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static com.app.ConcatNumbersByComma.*;
+
+public class ConcatNumbersByCommaShould {
+    @Test
+    public void return_3() {
+        assertThat(concatNumbers(asList(-1,-2)), equalTo("-1, -2"));
+    }
+}
